9795085ca3f2bdcf198463483dc5dc9dd5df7722
maurice-cubicit <maurice@cubicit.co.za>
2023-02-06 04:04:27 +0200

    feat: fixed image section on the login form

--- src/layout/Access.tsx
+++ src/layout/Access.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Form from '../componets/Form';
+import '../access.css'
 
 interface AccessProps {
     children: React.ReactNode;
@@ -10,9 +10,9 @@ export default function Access({children}: AccessProps) {
     <div className='w-full h-screen flex bg-blue-300'>
         <div className='w-9/12 m-auto flex bg-white rounded h-[85vh]'>
             <div className='w-1/2 bg-gradient-to-r from-blue-500 to-indigo-500 h-full realtive overflow-hidden hidden sm:block rounded-l' style={{position: 'relative',overflow:'hidden'}}>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className='cartoonImg'></div>
+                <div className='cloud_one'></div>
+                <div className='cloud_two'></div>
             </div>
             <div className='w-1/2'>
                 {children}
